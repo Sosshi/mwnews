62735d6e695689711d7daa67d1b7f158466c48e0
sosshi <symonmwenex@gmail.com>
2021-06-15 10:06:43 +0200

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>Home {{x}}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+    name: "Home",
+    data(){
+        return {
+            x:"hie there"
+        }
+    }
+
+});
+</script>
